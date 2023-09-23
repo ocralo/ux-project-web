f9074221face3a4327a96b5a8823a385ee2b29a9
ocralo <ocralo@gmail.com>
2023-09-23 17:34:46 -0500

    feat: create signup page

---
+++ src/app/pages/singup/singup.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'app-singup',
+	templateUrl: './singup.component.html',
+	styleUrls: ['./singup.component.scss'],
+})
+export class SingupComponent {}
---
+++ src/app/pages/singup/singup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { SingupRoutingModule } from './singup-routing.module'
+import { SingupComponent } from './singup.component'
+
+@NgModule({
+	declarations: [SingupComponent],
+	imports: [CommonModule, SingupRoutingModule],
+})
+export class SingupModule {}
