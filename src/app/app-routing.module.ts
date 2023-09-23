f025465cd2f1a954c41bb19be5164697e3c0f52f
ocralo <ocralo@gmail.com>
2023-09-23 17:35:09 -0500

    feat: create route to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+	{
+		path: 'login',
+		loadChildren: () =>
+			import('./pages/login/login.module').then((m) => m.LoginModule),
+	},
+	{
+		path: 'singup',
+		loadChildren: () =>
+			import('./pages/singup/singup.module').then((m) => m.SingupModule),
+	},
+	{
+		path: 'home',
+		loadChildren: () =>
+			import('./pages/home/home.module').then((m) => m.HomeModule),
+	},
+]
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
