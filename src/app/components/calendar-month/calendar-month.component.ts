5c37b6f6897dfd8b8c5bf109d134e944100d6a87
ocralo <rescobar@bancow.com.co>
2023-09-27 23:54:39 -0500

    feat: create calendars

---
+++ src/app/components/calendar-month/calendar-month.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'app-calendar-month',
+	templateUrl: './calendar-month.component.html',
+	styleUrls: ['./calendar-month.component.scss'],
+})
+export class CalendarMonthComponent {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
 import { HeaderComponent } from '../../components/header/header.component'
+import { CalendarViewComponent } from 'src/app/components/calendar-view/calendar-view.component'
+import { CalendarWeekComponent } from 'src/app/components/calendar-week/calendar-week.component'
+import { CalendarMonthComponent } from '../../components/calendar-month/calendar-month.component'
 
 @NgModule({
-	declarations: [HomeComponent, HeaderComponent],
-	imports: [CommonModule, HomeRoutingModule],
+	declarations: [
+		HomeComponent,
+		HeaderComponent,
+		CalendarViewComponent,
+		CalendarWeekComponent,
+		CalendarMonthComponent,
+	],
+	imports: [
+		CommonModule,
+		HomeRoutingModule,
+		MatButtonModule,
+		MatButtonToggleModule,
+	],
 })
 export class HomeModule {}
